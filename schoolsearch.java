e6b01505eba614f250a44efbe01e558a2970552b
Spencer Schurk <spencer@joyrun.com>
2018-09-24 11:04:37 -0700

    Added input loop with handlers for all search instructions

--- schoolsearch.java
+++ schoolsearch.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 
 public class schoolsearch {
 	private static ArrayList<Row> rows;
+	private static boolean quit;
 	
 	private static class Row {
 		public String StLastName;
@@ -19,59 +20,51 @@ public class schoolsearch {
 	}
 
 	public static void main(String[] args) {
+		quit = false;
 		// Init rows
 		rows = new ArrayList<Row>();
 
 		// Read file
 		try {
-			if (!initRows()) {
-				// initRows failed
-				return;
-			}
+			initRows();
+			System.out.println("File \'students.txt\' successfully loaded");
 		}
-		catch (IOException e) {
+		catch (Exception e) {
 			System.out.println("Error reading file");
 			return;
 		}
+
+		while (!quit) {
+			// Ask for input until user quits
+			System.out.println("Please enter a search instruction, or enter \'Q\' to quit");
+			handleInput();
+		}
+
+		//
 	}
 
-	private static boolean initRows() throws IOException {
+	private static void initRows() throws IOException, FileNotFoundException, NumberFormatException, Exception {
 		BufferedReader bufferedReader;
 		String line;
 
-		try {
-			FileReader fileReader = new FileReader("students.txt");
-			bufferedReader = new BufferedReader(fileReader);
-		}
-		catch (FileNotFoundException e) {
-			System.out.println("File does not exist in this directory");
-			return false;
-		}
+		FileReader fileReader = new FileReader("students.txt");
+		bufferedReader = new BufferedReader(fileReader);
 
 		while ((line = bufferedReader.readLine()) != null) {
 			String[] strings = line.split(",", 0);
 			Row temp = new Row();
 
 			if (strings.length != 8) {
-				System.out.println("Error with number of entries in row - " + strings.length);
-				return false;
+				bufferedReader.close();
+				throw new Exception();
 			}
 
 			temp.StLastName = strings[0];
 			temp.StFirstName = strings[1];
-			
-			try {
-				temp.Grade = Integer.parseInt(strings[2]);
-				temp.Classroom = Integer.parseInt(strings[3]);
-				temp.Bus = Integer.parseInt(strings[4]);
-				temp.GPA = Float.parseFloat(strings[5]);
-			}
-			catch (NumberFormatException e) {
-				System.out.println("Error reading number values from input file");
-				System.out.println("Line: " + line);
-				return false;
-			}
-
+			temp.Grade = Integer.parseInt(strings[2]);
+			temp.Classroom = Integer.parseInt(strings[3]);
+			temp.Bus = Integer.parseInt(strings[4]);
+			temp.GPA = Float.parseFloat(strings[5]);
 			temp.TLastName = strings[6];
 			temp.TFirstName = strings[7];
 
@@ -79,6 +72,76 @@ public class schoolsearch {
 		}
 
 		bufferedReader.close();
-		return true;
+	}
+
+	private static void handleInput() {
+		// Read in the whole user's input line and split it by spaces
+		String line = System.console().readLine();
+		String[] strings = line.split(" ", 0);
+
+		if (strings[0].equals("Q") || strings[0].equals("Quit")) {
+			// Quit the program
+			quit = true;
+		}
+		else if (strings[0].equals("I") || strings[0].equals("Info")) {
+			// Print info
+			// TODO
+		}
+		else if (strings[0].equals("A:") || strings[0].equals("Average:")) {
+			if (strings.length != 2) {
+				// Incorrect input
+				return;
+			}
+			// Run Average search
+			// TODO
+		}
+		else if (strings[0].equals("G:") || strings[0].equals("Grade:")) {
+			if (strings.length == 2) {
+				// Run Grade search without high / low flag
+				// TODO
+			}
+			else if (strings.length == 3 && (strings[2].equals("H") || strings[2].equals("High"))) {
+				// Run Grade search with high flag
+				// TODO
+			}
+			else if (strings.length == 3 && (strings[2].equals("L") || strings[2].equals("Low"))) {
+				// Run Grade search with low flag
+				// TODO
+			}
+			else {
+				// Incorrect input
+				return;
+			}
+		}
+		else if (strings[0].equals("B:") || strings[0].equals("Bus:")) {
+			if (strings.length != 2) {
+				// Incorrect input
+				return;
+			}
+			// Run Bus search
+			// TODO
+		}
+		else if (strings[0].equals("T:") || strings[0].equals("Teacher:")) {
+			if (strings.length != 2) {
+				// Incorrect input
+				return;
+			}
+			// Run Teacher search
+			// TODO
+		}
+		else if (strings[0].equals("S:") || strings[0].equals("Student:")) {
+			if (strings.length == 2) {
+				// Run Student search without Bus flag
+				// TODO
+			}
+			else if (strings.length == 3 && (strings[2].equals("B") || strings[2].equals("Bus"))) {
+				// Run Student search with Bus flag
+				// TODO
+			}
+			else {
+				// Incorrect input
+				return;
+			}
+		}
 	}
 }
